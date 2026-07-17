33df06a029388f14462096ecd1eb56a9f246f67f
avinashbatna24-afk <avinashbatna24@gmail.com>
2026-07-17 13:50:14 +0530

    Create FindClosestNumberToZero.java

---
+++ Arrays/FindClosestNumberToZero.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findClosestNumber(int[] nums) {
+        int closest = nums[0];
+        int distance = Integer.MAX_VALUE;
+        for(int i = 1;i<nums.length;i++){
+            if(Math.abs(nums[i])<Math.abs(closest))
+                closest = nums[i];
+            else if(Math.abs(nums[i])== Math.abs(closest)){
+                closest = Math.max(closest, nums[i]);
+            }
+
+        }
+        return closest;
