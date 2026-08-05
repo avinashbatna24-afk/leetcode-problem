f15db0abc6fed397840ec233b10d27cd2d56b237
avinashbatna24-afk <avinashbatna24@gmail.com>
2026-08-05 22:18:05 +0530

    Create Pascal'sTriangle.java

---
+++ Arrays/Pascal'sTriangle.java
@@ -0,0 +1,21 @@
+class Solution {
+    public static List<Integer> generateRow(int row){
+        int ans = 1;
+        List<Integer> ansRow = new ArrayList<>();
+        ansRow.add(1);
+        for(int c = 1;c<row;c++){
+            ans = ans *(row-c);
+            ans = ans/c;
+            ansRow.add(ans);
+        }
+        return ansRow;
+    }
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i = 1;i<=numRows;i++){
+            ans.add(generateRow(i));
+        }
+        return ans;
+
+    }
+}
